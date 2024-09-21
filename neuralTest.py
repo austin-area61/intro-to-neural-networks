d2f8a4c78a6def9ea838cbc557dbf05ed2b39216
austin-area61 <austinonyango.area61@gmail.com>
2024-09-21 19:56:32 +0300

    added inputs for temperature and weather (isSunny or not)

--- neuralTest.py
+++ neuralTest.py
@@ -2,3 +2,13 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+
+#inputs: temperature, isSunny(1 for it being sunny, 0 for not sunny)
+x = np.array([
+  [30, 1],
+  [10, 0],
+  [20, 1],
+  [15, 0],
+  [25, 1],
+  [5, 0]
+])
